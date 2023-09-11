6f918773a58d664d7ce07c899ce0ad57f0dcbd2d
Horaciomdp <horazopo@gmail.com>
2023-09-11 14:52:37 -0300

    api prueba

--- js/main.js
+++ js/main.js
@@ -2,3 +2,38 @@ window.addEventListener("load", ()=>{
     const currentDate= new Date();
     document.getElementById("currentYear").innerText=currentDate.getFullYear(); 
 })
+
+
+//variables
+const url = 'https://jsonplaceholder.typicode.com/users';
+const respuesta = document.querySelector('#respuesta');
+
+
+//evento
+
+document.addEventListener("DOMContentLoaded", llamarAPI);
+
+//funcion
+
+
+function llamarAPI(){
+    console.log("corriendo");
+    fetch (url)
+        .then(resp => resp.json())
+        .then(function(data){
+            mostrarHTML(data);
+        } )   
+
+}
+
+function mostrarHTML(datos){
+
+   datos.forEach(item => {
+        const row = document.createElement('tr');
+        row.innerHTML =` 
+        <td>${item.name}</td>
+        <td>${item.email} </td>
+        `
+        respuesta.appendChild(row);
+   }); 
+}
